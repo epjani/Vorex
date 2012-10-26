75e073381de656446821d6e7d1a1a00691dea095
epjani <edin.pjanic@atlantbh.com>
2012-10-26 11:05:48 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vorex::Application.config.secret_token = '22d18e7a7eb4c448c80c17e0a6dc7e45f8988eebc4735aab2ce469ab7f861d4d6f893542c57c71aff1acd6b1a50fda1931400658c3060da60d6945c17569477a'
